8f301ee85ffeb57a00a7c31047b6102c00f8f967
Gamir07 <gamir07@yandex.ru>
2023-09-29 00:15:01 +0300

    Сделал ДЗ

---
+++ src/main/java/pro/sky/skyprospringcalculator/CalculatorServiceImpl.java
@@ -0,0 +1,22 @@
+package pro.sky.skyprospringcalculator;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CalculatorServiceImpl implements CalculatorService{
+    public String plus(int num1,int num2) {
+        return num1 + " + " + num2 + " = " + (num1 + num2);
+    }
+
+    public String minus(int num1,int num2) {
+        return num1 + " - " + num2 + " = " + (num1 - num2);
+    }
+
+    public String multiply(int num1, int num2) {
+        return num1 + " * " + num2 + " = " + (num1 * num2);
+    }
+
+    public String divide(int num1, int num2) {
+        return num1 + " / " + num2 + " = " + (num1 / num2);
+    }
+}
